165fb450b694b94f65420ddb3844682707ef7e7a
Grant Gryczan <grantgryczan@gmail.com>
2022-03-11 18:40:24 -0500

    Finish sprint 4

--- scripts/data.js
+++ scripts/data.js
@@ -2,11 +2,15 @@ export let data = {};
 
 export const load = () => {
 	try {
-		// data = JSON.parse(localStorage.data);
+		data = JSON.parse(localStorage.data);
 	} catch {}
 
 	if (data.weights === undefined) {
 		data.weights = [];
+	} else {
+		for (const weight of data.weights) {
+			weight.date = new Date(weight.date);
+		}
 	}
 };
 
@@ -15,3 +19,11 @@ load();
 export const save = () => {
 	localStorage.data = JSON.stringify(data);
 };
+
+const resetDataOption = document.getElementById('reset-data-option');
+
+resetDataOption.addEventListener('click', () => {
+	data = {};
+	save();
+	location.reload();
+});
