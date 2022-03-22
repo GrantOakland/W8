fc141afdb0b3a1efbdc990bba91a02c847e1dc17
Grant Gryczan <grantgryczan@gmail.com>
2022-03-22 18:06:17 -0400

    Import and export

--- scripts/data.js
+++ scripts/data.js
@@ -5,6 +5,8 @@ const data = new UserData();
 export default data;
 
 const resetDataOption = document.getElementById('reset-data-option');
+const exportDataOption = document.getElementById('export-data-option');
+const importDataOption = document.getElementById('import-data-option');
 
 resetDataOption.addEventListener('click', () => {
 	if (!confirm('Are you sure you want to reset all your data?\n\nThis cannot be undone.')) {
@@ -12,4 +14,40 @@ resetDataOption.addEventListener('click', () => {
 	}
 
 	data.reset();
+});
+
+exportDataOption.addEventListener('click', () => {
+	const a = document.createElement('a');
+	const blob = new Blob([JSON.stringify(data)], {
+		type: 'application/json'
+	});
+	a.href = URL.createObjectURL(blob);
+	a.download = 'w8.json';
+	a.click();
+});
+
+const fileInput = document.createElement('input');
+fileInput.type = 'file';
+fileInput.accept = 'application/json';
+
+importDataOption.addEventListener('click', () => {
+	fileInput.click();
+});
+
+fileInput.addEventListener('change', () => {
+	const reader = new FileReader();
+
+	reader.addEventListener('load', () => {
+		try {
+			JSON.parse(reader.result);
+		} catch {
+			alert('An error occurred while importing.');
+		}
+
+		localStorage.data = reader.result;
+		alert('Data imported successfully!');
+		location.reload();
+	});
+
+	reader.readAsText(fileInput.files[0]);
 });
